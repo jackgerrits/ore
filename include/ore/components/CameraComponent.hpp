e337669403a85a5a5444621990be22b25796ddea
Jack Gerrits <jackgerrits95@gmail.com>
2017-02-12 10:52:12 +1030

    Added various components

---
+++ include/ore/components/CameraComponent.hpp
@@ -0,0 +1,31 @@
+#ifndef CAMERA_COMPONENT_H
+#define CAMERA_COMPONENT_H
+
+#define _USE_MATH_DEFINES
+
+#include <ore/components/Component.hpp>
+#include <ore/Model.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <vector>
+
+namespace ore {
+    class CameraComponent : public BaseComponent<CameraComponent> {
+    public:
+        glm::vec3 position;
+        glm::vec3 focalPoint;
+
+        glm::mat4 view;
+        glm::mat4 projection;
+
+        CameraComponent() {
+
+        }
+
+        // CameraComponent(glm::vec3 position, glm::vec3 focalPoint, glm::mat4 projection)
+        //     : position(position), focalPoint(focalPoint), projection(projection) {}
+    };
+}
+
+#endif
---
+++ include/ore/components/LightComponent.hpp
@@ -0,0 +1,22 @@
+#ifndef LIGHT_COMPONENT_H
+#define LIGHT_COMPONENT_H
+
+#define _USE_MATH_DEFINES
+
+#include <ore/components/Component.hpp>
+
+#include <glm/ext.hpp>
+
+namespace ore {
+    struct LightComponent : public BaseComponent<LightComponent> {
+        glm::vec4 position; // Position of the camera in world coordinates. w value of 0 indicates directional light, 1 indicates positional/point light
+        glm::vec3 specular; // Colour intensities of each light component
+        glm::vec3 diffuse;
+        glm::vec3 ambient;
+        float radius;           // Radius used for attenuation
+        float coneAngle;        // Angle and direction of cone for spotlights
+        glm::vec3 coneDirection;
+    };
+}
+
+#endif
