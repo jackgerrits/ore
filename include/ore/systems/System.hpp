e8ce02f0fe31f70c9eb771abdfdd271a0d903d35
Jack Gerrits <jackgerrits95@gmail.com>
2017-02-12 10:49:10 +1030

    removed type id

--- include/ore/systems/System.hpp
+++ include/ore/systems/System.hpp
@@ -5,45 +5,19 @@
 
 #include <ore/components/Component.hpp>
 #include <ore/Entity.hpp>
+#include <ore/EntityManager.hpp>
 
 #include <glm/ext.hpp>
 #include <vector>
 
 namespace ore {
-    // Accepts a variadic template of types and returns a vector of their corresponding ids.
-    struct IdTypeResolver {
-        template<class T, class... Args>
-        std::vector<int> build() {
-            contents.push_back(componentId<T>());
-            return build<Args...>();
-        }
-
-        template<class T>
-        std::vector<int> build()
-        {
-            contents.push_back(componentId<T>());
-            return contents;
-        }
-
-        std::vector<int> contents;
-    };
-
-    template<typename... Args>
     class System {
     public:
-        const std::vector<int> requiredComponents;
-
         System() {
-            IdTypeResolver resolver;
-            requiredComponents = resolver.build<Args...>();
         }
 
         // Systems will always be given a list of all components that fullfil the requirements.
-        virtual void visit(std::vector<Entity*> entities) {
-            for(auto entity : entities) {
-                visit(entity);
-            }
-        }
+        virtual void process(EntityManager* entityManager) = 0;
     };
 }
 
